b60964eb13f3736df9008e78b91cc524c8c8cd2d
MYassineBZ <mohamed-yassine.bouzaiene@univ-tlse3.fr>
2024-09-26 09:51:49 +0200

    fin seance 1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -14,6 +14,76 @@ public class Village {
 		villageois = new Gaulois[nbVillageoisMaximum];
 	}
 
+	private static class Marche {
+		private Etal[] etals;
+
+		private Marche(int nbEtal) {
+			this.etals = new Etal[nbEtal];
+			for (int i = 0; i < nbEtal; i++) {
+				this.etals[i] = new Etal();
+			}
+		}
+
+		private void utiliserEtal(int indiceEtal, Gaulois Vendeur, String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(Vendeur, produit, nbProduit);
+		}
+
+		private int trouverEtalLibre() {
+			int longueur = this.etals.length;
+			for (int cpt = 0; cpt != longueur - 1; cpt++) {
+				if (!this.etals[cpt].isEtalOccupe()) {
+					return cpt;
+				}
+
+			}
+			return -1;
+
+		}
+
+		private Etal[] trouverEtals(String produit) {
+			int nbproduit = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (this.etals[i].contientProduit(produit)) {
+					nbproduit++;
+				}
+
+			}
+			Etal[] VendProduit = new Etal[nbproduit];
+
+			for (int j = 0, k = 0; j < etals.length; j++) {
+				if (etals[j].contientProduit(produit)) {
+					VendProduit[k] = etals[j];
+					k++;
+				}
+
+			}
+			return VendProduit;
+		}
+
+		private Etal trouverVendeur(Gaulois gaulois) {
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].getVendeur().equals(gaulois)) {
+					return etals[i];
+				}
+			}
+			return null;
+		}
+
+		private void afficheMarche() {
+			int nbEtal = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (!this.etals[i].isEtalOccupe()) {
+					nbEtal++;
+				}
+			}
+
+			if (nbEtal == 0) {
+				System.out.println("Il ne reste aucun étals libre \n");
+			}
+			System.out.println("Il reste" + nbEtal + "étals non utilisés dans le marché\n");
+		}
+	}
+
 	public String getNom() {
 		return nom;
 	}
@@ -45,15 +115,14 @@ public class Village {
 	public String afficherVillageois() {
 		StringBuilder chaine = new StringBuilder();
 		if (nbVillageois < 1) {
-			chaine.append("Il n'y a encore aucun habitant au village du chef "
-					+ chef.getNom() + ".\n");
+			chaine.append("Il n'y a encore aucun habitant au village du chef " + chef.getNom() + ".\n");
 		} else {
-			chaine.append("Au village du chef " + chef.getNom()
-					+ " vivent les légendaires gaulois :\n");
+			chaine.append("Au village du chef " + chef.getNom() + " vivent les légendaires gaulois :\n");
 			for (int i = 0; i < nbVillageois; i++) {
 				chaine.append("- " + villageois[i].getNom() + "\n");
 			}
 		}
 		return chaine.toString();
 	}
+
 }
